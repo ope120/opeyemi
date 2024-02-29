870d17c8a93ec5a22d13931adb488f06abd054d1
ope120 <161703232+ope120@users.noreply.github.com>
2024-02-29 10:43:29 +0100

    Update javascript.js

---
+++ javascript.js
@@ -0,0 +1,14 @@
+ .timer-container {
+    display: flex;
+    justify-content: center;
+  }
+
+  .time-box {
+    margin-top: 50px;
+    background-color: #dadce6a2;
+    padding: 50px;
+    border-radius: 10px;
+    margin: 5px;
+    font-size: 16px;
+    text-align: center;
+  }
